49eaaffa6a51578f1eed8d405d00652705127dde
xyMerlinxy <xymerlinxy@gmail.com>
2021-09-13 21:42:01 +0200

    Deleting unnecessary files

--- gui_file/compile.bat
+++
@@ -1,6 +0,0 @@
-pyuic6 -x Main_Window.ui -o Main_Window.py
-pyuic6 -x Size_Change_Widget.ui -o Size_Change_Widget.py
-pyuic6 -x Data_Export.ui -o Data_Export.py
-pyuic6 -x Data_Import.ui -o Data_Import.py
-pyuic6 -x Import_Error.ui -o Import_Error.py
-pause
